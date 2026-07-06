52e23bfe50f1abdb082fb436de70426fe3feecfb
luis.capelletto <luis.capelletto@dataprev.gov.br>
2026-07-06 18:35:59 -0300

    remove insta + testimonials

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,89 +1,85 @@
 import { footer_square, upduo_logo_white } from '@constants'
 import Image from 'next/future/image'
-import { BsBehance, BsInstagram, BsWhatsapp } from 'react-icons/bs'
+import { BsBehance, BsWhatsapp } from 'react-icons/bs'
 import { MdOutlineMail } from 'react-icons/md'
 import {
-  FooterContainer,
-  SocialContainer,
-  SocialLink,
-  SocialLinks,
-  SquareLeft,
-  SquareRight,
-  UpduoContainer,
+	FooterContainer,
+	SocialContainer,
+	SocialLink,
+	SocialLinks,
+	SquareLeft,
+	SquareRight,
+	UpduoContainer,
 } from './styles'
 
 export function Footer() {
-  const currentYear = new Date().getFullYear()
+	const currentYear = new Date().getFullYear()
 
-  return (
-    <FooterContainer>
-      <SquareLeft>
-        <Image src={footer_square} alt="Quadrados azuis." />
-      </SquareLeft>
+	return (
+		<FooterContainer>
+			<SquareLeft>
+				<Image src={footer_square} alt='Quadrados azuis.' />
+			</SquareLeft>
 
-      <SquareRight>
-        <Image src={footer_square} alt="Quadrados azuis." />
-      </SquareRight>
+			<SquareRight>
+				<Image src={footer_square} alt='Quadrados azuis.' />
+			</SquareRight>
 
-      <UpduoContainer>
-        <Image src={upduo_logo_white} alt="UpDuo Logo." />
+			<UpduoContainer>
+				<Image src={upduo_logo_white} alt='UpDuo Logo.' />
 
-        <span>
-          © {currentYear} UpDuo Comunicação. <br />
-          Todos os direitos reservados.
-        </span>
-      </UpduoContainer>
+				<span>
+					© {currentYear} UpDuo Comunicação. <br />
+					Todos os direitos reservados.
+				</span>
+			</UpduoContainer>
 
-      <SocialContainer>
-        <SocialLinks>
-          <SocialLink
-            href="mailto:contato.upduo@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            title="Enviar e-mail para UpDuo"
-          >
-            <MdOutlineMail size={32} color="#fff" />
-          </SocialLink>
+			<SocialContainer>
+				<SocialLinks>
+					<SocialLink
+						href='mailto:contato.upduo@gmail.com'
+						target='_blank'
+						rel='noopener noreferrer nofollow'
+						title='Enviar e-mail para UpDuo'
+					>
+						<MdOutlineMail size={32} color='#fff' />
+					</SocialLink>
 
-          <SocialLink
-            href="https://wa.me/5561992319872"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            title="WhatsApp da UpDuo"
-          >
-            <BsWhatsapp size={26} color="#fff" />
-          </SocialLink>
+					<SocialLink
+						href='https://wa.me/5561992319872'
+						target='_blank'
+						rel='noopener noreferrer nofollow'
+						title='WhatsApp da UpDuo'
+					>
+						<BsWhatsapp size={26} color='#fff' />
+					</SocialLink>
 
-          <SocialLink
+					{/* <SocialLink
             href="https://www.instagram.com/upduocomunicacao"
             target="_blank"
             rel="noopener noreferrer nofollow"
             title="Ver Instagram da UpDuo"
           >
             <BsInstagram size={28} color="#fff" />
-          </SocialLink>
+          </SocialLink> */}
 
-          <SocialLink
-            href="https://www.behance.net/sabrinecruz"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            title="Ver Behance da UpDuo"
-          >
-            <BsBehance size={32} color="#fff" />
-          </SocialLink>
-        </SocialLinks>
+					<SocialLink
+						href='https://www.behance.net/sabrinecruz'
+						target='_blank'
+						rel='noopener noreferrer nofollow'
+						title='Ver Behance da UpDuo'
+					>
+						<BsBehance size={32} color='#fff' />
+					</SocialLink>
+				</SocialLinks>
 
-        <span>
-          Feito com 🤍 por{' '}
-          <a
-            href="https://github.com/capelaum"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            Luís Vinicius Capelletto
-          </a>
-        </span>
-      </SocialContainer>
-    </FooterContainer>
-  )
+				<span>
+					Feito com 🤍 por{' '}
+					<a href='https://github.com/capelaum' target='_blank' rel='noopener noreferrer nofollow'>
+						Luís Vinicius Capelletto
+					</a>
+				</span>
+			</SocialContainer>
+		</FooterContainer>
+	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { Portfolio } from 'components/Portfolio'
 import { Services } from 'components/Services'
 import { ServicesInfoItems } from 'components/ServicesInfoItems'
 import { Solutions } from 'components/Solutions'
-import { Testimonials } from 'components/Testimonials'
 import { DatoCmsRequest } from 'graphql/datocms'
 import { GET_HOME } from 'graphql/queries/getHome'
 import type { GetStaticProps } from 'next'
@@ -16,92 +15,87 @@ import { HomeContainer, HomeContent } from 'styles/pages/home'
 import { Project, Testimonial } from 'types/home'
 
 interface HomeProps {
-  allTestimonials: Testimonial[]
-  homeProjects: Project[]
+	allTestimonials: Testimonial[]
+	homeProjects: Project[]
 }
 
 export default function Home({ allTestimonials, homeProjects }: HomeProps) {
-  const [isServicesInView, setIsServicesInView] = useState(false)
-  const [isTestimonialsInView, setIsTestimonialsInView] = useState(false)
-  const [isPortfolioInView, setIsPortfolioInView] = useState(false)
+	const [isServicesInView, setIsServicesInView] = useState(false)
+	const [isTestimonialsInView, setIsTestimonialsInView] = useState(false)
+	const [isPortfolioInView, setIsPortfolioInView] = useState(false)
 
-  const setServicesInView = useCallback((state: boolean) => {
-    setIsServicesInView(state)
-  }, [])
+	const setServicesInView = useCallback((state: boolean) => {
+		setIsServicesInView(state)
+	}, [])
 
-  const setTestimonialsInView = useCallback((state: boolean) => {
-    setIsTestimonialsInView(state)
-  }, [])
+	const setTestimonialsInView = useCallback((state: boolean) => {
+		setIsTestimonialsInView(state)
+	}, [])
 
-  const setPortfolioInView = useCallback((state: boolean) => {
-    setIsPortfolioInView(state)
-  }, [])
+	const setPortfolioInView = useCallback((state: boolean) => {
+		setIsPortfolioInView(state)
+	}, [])
 
-  return (
-    <>
-      <Head>
-        <title>UpDuo Comunicação</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>UpDuo Comunicação</title>
+			</Head>
 
-      <Header
-        sectionsInView={{
-          isServicesInView,
-          isTestimonialsInView,
-          isPortfolioInView,
-        }}
-      />
+			<Header
+				sectionsInView={{
+					isServicesInView,
+					isTestimonialsInView,
+					isPortfolioInView,
+				}}
+			/>
 
-      <HomeContainer>
-        <HomeContent>
-          <Hero />
-          <ServicesInfoItems />
-          <Solutions />
-          <Services id="services" setServicesInView={setServicesInView} />
-          <Testimonials
+			<HomeContainer>
+				<HomeContent>
+					<Hero />
+					<ServicesInfoItems />
+					<Solutions />
+					<Services id='services' setServicesInView={setServicesInView} />
+					{/* <Testimonials
             id="testimonials"
             testimonials={allTestimonials}
             setTestimonialsInView={setTestimonialsInView}
-          />
-          <Portfolio
-            id="portfolio"
-            homeProjects={homeProjects}
-            setPortfolioInView={setPortfolioInView}
-          />
-          <CallToAction />
-          <Footer />
-        </HomeContent>
-      </HomeContainer>
-    </>
-  )
+          /> */}
+					<Portfolio id='portfolio' homeProjects={homeProjects} setPortfolioInView={setPortfolioInView} />
+					<CallToAction />
+					<Footer />
+				</HomeContent>
+			</HomeContainer>
+		</>
+	)
 }
 
 interface ResponseDataProps {
-  allTestimonials: Testimonial[]
-  allProjects: Project[]
+	allTestimonials: Testimonial[]
+	allProjects: Project[]
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  try {
-    const { allTestimonials, allProjects: homeProjects }: ResponseDataProps =
-      await DatoCmsRequest({
-        query: GET_HOME,
-        variables: {
-          first: 3,
-        },
-      })
+	try {
+		const { allTestimonials, allProjects: homeProjects }: ResponseDataProps = await DatoCmsRequest({
+			query: GET_HOME,
+			variables: {
+				first: 3,
+			},
+		})
 
-    return {
-      props: {
-        allTestimonials,
-        homeProjects,
-      },
-      revalidate: 30,
-    }
-  } catch (error) {
-    console.log('🚀 ~ error', error)
+		return {
+			props: {
+				allTestimonials,
+				homeProjects,
+			},
+			revalidate: 30,
+		}
+	} catch (error) {
+		console.log('🚀 ~ error', error)
 
-    return {
-      notFound: true,
-    }
-  }
+		return {
+			notFound: true,
+		}
+	}
 }
